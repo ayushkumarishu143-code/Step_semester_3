310d6471c34127d41d888435223d6f7bc026eabb
ayushkumarishu143-code <ayushkumar.ishu143@gmail.com>
2026-08-22 10:48:13 +0530

    feat: add category C solutions under src/main/java/java_control_flow/assigment_problems

---
+++ src/main/java/java_control_flow/assigment_problems/PalindromeCheck.java
@@ -0,0 +1,25 @@
+package java_control_flow.assigment_problems;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int origNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        if (origNumber == reversedNumber) {
+            System.out.println(origNumber + " is a Palindrome.");
+        } else {
+            System.out.println(origNumber + " is not a Palindrome.");
+        }
+        sc.close();
+    }
+}
